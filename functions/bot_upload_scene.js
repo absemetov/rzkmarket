47e0735915de4a10b016b20e75fc69ebe06da9a1
Nadir <nadir@absemetov.org.ua>
2021-06-05 23:44:23 +0300

    test upload change timeout

--- functions/bot_upload_scene.js
+++ functions/bot_upload_scene.js
@@ -39,46 +39,48 @@ upload.on("text", async (ctx) => {
       ctx.replyWithMarkdown(`Load goods from sheet *${doc.title + " with " + (sheet.rowCount - 1)}* rows`);
       // read rows
 
-      const perPage = 10;
-      const rowCount = 10; // sheet.rowCount
+      const perPage = 100;
+      const rowCount = sheet.rowCount;
+      let countUploadGoods = 0;
       for (let i = 0; i < rowCount - 1; i += perPage) {
         console.log(`rowCount ${sheet.rowCount - 1}, limit: ${perPage}, offset: ${i}`);
         const rows = await sheet.getRows({limit: perPage, offset: i});
-        const BreakException = {};
-        try {
-          rows.forEach(async (row) => {
-            // validate data if ID and NAME set org Name and PRICE
-            const item = {
-              id: row.id,
-              name: row.name,
-              price: row.price ? Number(row.price.replace(",", ".")) : "",
-            };
-            const rulesItemRow = {
-              id: "required|alpha_dash",
-              name: "required|string",
-              price: "required|numeric",
-            };
-            const validateItemRow = new Validator(item, rulesItemRow);
-            if (validateItemRow.fails() && ((row.id && row.name) || (row.name && row.price))) {
-              BreakException.rowIndex = row.rowIndex;
-              BreakException.desc = validateItemRow.errors.all();
-              throw BreakException;
+
+        for (let j = 0; j < rows.length; j++) {
+          // validate data if ID and NAME set org Name and PRICE
+          const item = {
+            id: rows[j].id,
+            name: rows[j].name,
+            price: rows[j].price ? Number(rows[j].price.replace(",", ".")) : "",
+          };
+          const rulesItemRow = {
+            id: "required|alpha_dash",
+            name: "required|string",
+            price: "required|numeric",
+          };
+          const validateItemRow = new Validator(item, rulesItemRow);
+          if (validateItemRow.fails() && ((rows[j].id && rows[j].name) || (rows[j].name && rows[j].price))) {
+            for (const [key, error] of Object.entries(validateItemRow.errors.all())) {
+              ctx.replyWithMarkdown(`Row *${rows[j].rowIndex}* Column *${key}* Error *${error}*`);
             }
-            // save data to firestore
-            if (validateItemRow.passes()) {
-              console.log(item);
-              await firebase.firestore().collection("products").doc(item.id).set({
-                "name": item.name,
-                "price": item.price,
-              });
-            }
-          });
-        } catch (error) {
-          console.log(error);
-          for (const [key, value] of Object.entries(error.desc)) {
-            ctx.replyWithMarkdown(`Row *${error.rowIndex}* Column *${key}* Error *${value}*`);
+            // disable parent loop
+            i = rowCount;
+            break;
+          }
+          // save data to firestore
+          if (validateItemRow.passes()) {
+            countUploadGoods++;
+            await firebase.firestore().collection("products").doc(item.id).set({
+              "name": item.name,
+              "price": item.price,
+            });
           }
         }
+        ctx.replyWithMarkdown(`*${i + perPage}* rows scan from *${sheet.rowCount - 1}*`);
+      }
+      // show count upload goods
+      if (countUploadGoods) {
+        ctx.replyWithMarkdown(`*${countUploadGoods}* goods uploaded`);
       }
     } catch (error) {
       ctx.replyWithMarkdown(`Sheet Error *${error}*`);
