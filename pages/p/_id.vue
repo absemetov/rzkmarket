b4d7237e6ebfd8566672fb6c981b081b01ec5994
Nadir <nadir@absemetov.org.ua>
2021-06-22 01:05:02 +0300

    goods paginate 1.0

---
+++ pages/p/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-alert>
+    <h1>
+      Product {{ product }} {{ product.name }} {{ product.price }}
+    </h1>
+  </v-alert>
+</template>
+<script>
+export default {
+  async asyncData ({ params, $fire }) {
+    const doc = await $fire.firestore.collection('products').doc(params.id).get()
+    const product = { id: doc.id, ...doc.data() }
+    return { product }
+  }
+}
+</script>
--- pages/p/index.vue
+++ pages/p/index.vue
@@ -1,22 +1,45 @@
 <template>
-  <v-alert type="success">
+  <v-alert>
     <ul>
       <li v-for="product of products" :key="product.id">
-        <h1>{{ product.id }} {{ product.name }}  {{ product.price }}</h1>
+        <h1>
+          <NuxtLink :to="{ name: 'p-id', params: { id: product.id } }">
+            {{ product.id }} {{ product.name }}  {{ product.price }}
+          </NuxtLink>
+        </h1>
       </li>
     </ul>
+    <button @click="paginate">
+      Refresh
+    </button>
+    <NuxtLink :to="{ name: 'p', query: { startAfter: lastDoc.id }}">
+      Next {{ query.startAfter }}
+    </NuxtLink>
   </v-alert>
 </template>
 <script>
 export default {
-  async asyncData ({ params, $http, $fire }) {
+  async asyncData ({ params, query, $http, $fire }) {
     const productsSnapshot = await $fire.firestore.collection('products').limit(10).get()
-
+    const lastDoc = productsSnapshot.docs[productsSnapshot.docs.length - 1]
+    // Keep track of the last loaded document.
     const products = productsSnapshot.docs.map((doc) => {
       return { id: doc.id, ...doc.data() }
     })
-
-    return { products }
+    console.info(query)
+    // const productsSnapshot = await $fire.firestore.collection('products').orderBy('name').startAfter('21111025').limit(10).get()
+    // const productsSnapshot1 = await $fire.firestore.collection('products').limit(10).startAfter(lastDoc).get()
+    // const productsSnapshot = await $fire.firestore.collection('products').orderBy('name').startAfter('21111025').limit(10).get()
+    // Keep track of the last loaded document.
+    // const products = productsSnapshot1.docs.map((doc) => {
+    //   return { id: doc.id, ...doc.data() }
+    // })
+    return { products, lastDoc, query }
+  },
+  methods: {
+    paginate () {
+      alert('tetet')
+    }
   }
 }
 </script>
