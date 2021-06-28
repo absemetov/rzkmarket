db9658758f7ce411cc10a52f10dd2b3b2862f91d
Nadir <nadir@absemetov.org.ua>
2021-06-28 23:58:36 +0300

    Back button click

--- pages/p/index.vue
+++ pages/p/index.vue
@@ -1,19 +1,19 @@
 <template>
   <v-alert>
-    <p v-if="$fetchState.pending">
-      <span class="loading" />
-    </p>
     <ul>
       <li v-for="product of products" :key="product.id">
         <h1>
           <NuxtLink :to="{ name: 'p-id', params: { id: product.id } }">
-            {{ product.id }} {{ product.name }}  {{ product.price }}
+            {{ product.i }} {{ product.id }} {{ product.name }}  {{ product.price }}
           </NuxtLink>
         </h1>
       </li>
     </ul>
+    <p v-if="$fetchState.pending">
+      <span class="loading" />
+    </p>
     <NuxtLink v-if="nextProductId" :to="{ name: 'p', query: { startAfter: nextProductId }}">
-      Next
+      Load More
     </NuxtLink>
   </v-alert>
 </template>
@@ -23,12 +23,13 @@ export default {
     return {
       products: [],
       nextProductId: null,
-      lastProduct: null
+      lastProduct: null,
+      i: 1
     }
   },
   async fetch () {
     let query = this.$fire.firestore.collection('products').limit(10)
-    // get lastProduct if reload page
+    // get lastProduct if reload page server side
     if (this.$route.query.startAfter && !this.lastProduct) {
       // get lastProduct
       const lastProduct = await this.$fire.firestore.collection('products').doc(this.$route.query.startAfter).get()
@@ -37,40 +38,53 @@ export default {
       console.log('server side', lastProduct)
     }
     // in client side
+    // if use back browser check ids
+    let backButtonClick = false
     if (this.$route.query.startAfter && this.lastProduct) {
+      if (this.lastProduct.id !== this.$route.query.startAfter) {
+        backButtonClick = true
+      }
       // eslint-disable-next-line no-console
-      console.log('client side', this.lastProduct)
+      console.log('client side', this.lastProduct.id, this.$route.query.startAfter)
       query = query.startAfter(this.lastProduct)
     }
     // get all products
-    const productsSnapshot = await query.get()
-    // get next product ID
-    if (productsSnapshot.size < 10) {
-      this.nextProductId = null
-    } else {
-      this.nextProductId = productsSnapshot.docs[productsSnapshot.docs.length - 1].id
-      // in client side save lastProduct
-      if (process.client) {
-        this.lastProduct = productsSnapshot.docs[productsSnapshot.docs.length - 1]
-        // eslint-disable-next-line no-console
-        console.log('client side lastProduct changed', this.lastProduct)
+    if (!backButtonClick) {
+      const productsSnapshot = await query.get()
+
+      // get next product ID
+      if (productsSnapshot.size < 10) {
+        this.nextProductId = null
+      } else {
+        this.nextProductId = productsSnapshot.docs[productsSnapshot.docs.length - 1].id
+        // in client side save lastProduct
+        if (process.client) {
+          this.lastProduct = productsSnapshot.docs[productsSnapshot.docs.length - 1]
+          // eslint-disable-next-line no-console
+          console.log('client side lastProduct changed', this.lastProduct)
+        }
+      }
+      // generate products array
+      // this.products = productsSnapshot.docs.map((doc) => {
+      //   return { id: doc.id, ...doc.data() }
+      // })
+      // load more
+      // if show index page clear products
+      if (process.client && !this.$route.query.startAfter) {
+        this.products = []
+      }
+      for (const doc of productsSnapshot.docs) {
+        // if use back button check exist items
+        const found = this.products.some(el => el.id === doc.id)
+        if (!found) {
+          this.products.push({
+            id: doc.id,
+            i: this.i++,
+            ...doc.data()
+          })
+        }
       }
     }
-    // generate products array
-    this.products = productsSnapshot.docs.map((doc) => {
-      return { id: doc.id, ...doc.data() }
-    })
-    // load more
-    // for (const doc of productsSnapshot.docs) {
-    //   // if use back button check exist items
-    //   const found = this.products.some(el => el.id === doc.id)
-    //   if (!found) {
-    //     this.products.push({
-    //       id: doc.id,
-    //       ...doc.data()
-    //     })
-    //   }
-    // }
   },
   watch: {
     '$route.query': '$fetch'
