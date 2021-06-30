717deedc16918881c23568102df8935254d7a883
Nadir Absemetov <nadir@absemetov.org.ua>
2021-06-30 14:11:51 +0300

    Goods paginate final 1.1

--- pages/p/index.vue
+++ pages/p/index.vue
@@ -12,11 +12,8 @@
     <p v-if="$fetchState.pending">
       <span class="loading" />
     </p>
-    <NuxtLink v-if="nextProductId" :to="{ name: 'p', query: { endBefore: nextProductId }}">
-      Prev 10
-    </NuxtLink>
-    <NuxtLink v-if="nextProductId" :to="{ name: 'p', query: { startAfter: nextProductId }}">
-      Next 10
+    <NuxtLink v-if="nextProductId" :prefetch="false" :to="{ name: 'p', query: { startAfter: nextProductId }}">
+      Load more ...
     </NuxtLink>
   </v-alert>
 </template>
@@ -26,7 +23,8 @@ export default {
     return {
       products: [],
       nextProductId: null,
-      snapshots: []
+      lastProduct: null,
+      lastPage: false
     }
   },
   async fetch () {
@@ -34,21 +32,26 @@ export default {
     // make query Next link
     if (this.$route.query.startAfter) {
       let lastProduct = null
-      // // in client side
-      if (this.snapshots[this.$route.query.startAfter]) {
-        lastProduct = this.snapshots[this.$route.query.startAfter]
+      // // in client side or last page next false!
+      if (this.lastProduct || this.lastPage) {
+        // back button click
+        if (this.$route.query.startAfter !== this.nextProductId) {
+          // eslint-disable-next-line no-console
+          console.log('back button detect', this.$route.query.startAfter, this.nextProductId)
+          this.products = []
+          lastProduct = await this.$fire.firestore.collection('products').doc(this.$route.query.startAfter).get()
+          // return false
+        } else {
+          // eslint-disable-next-line no-console
+          console.log('snap detect +++', this.$route.query.startAfter, this.nextProductId)
+          lastProduct = this.lastProduct
+        }
       } else {
+        // server side get last snapshot
+        // eslint-disable-next-line no-console
+        console.log('server res', this.$route.query.startAfter, this.nextProductId)
         lastProduct = await this.$fire.firestore.collection('products').doc(this.$route.query.startAfter).get()
       }
-      // if back button click
-      //   if (this.$route.query.startAfter === this.lastProduct.id) {
-      //     lastProduct = this.lastProduct
-      //   } else {
-      //     lastProduct = await this.$fire.firestore.collection('products').doc(this.$route.query.startAfter).get()
-      //   }
-      // } else {
-      // get lastProduct on server
-      // }
       query = query.startAfter(lastProduct)
     }
     // get query prodycts
@@ -56,34 +59,26 @@ export default {
 
     // get next product ID
     if (productsSnapshot.size < 10) {
+      this.lastPage = true
       this.nextProductId = null
     } else {
       this.nextProductId = productsSnapshot.docs[productsSnapshot.docs.length - 1].id
       // in client side save lastProduct
       if (process.client) {
-        this.snapshots[this.nextProductId] = productsSnapshot.docs[productsSnapshot.docs.length - 1]
+        this.lastProduct = productsSnapshot.docs[productsSnapshot.docs.length - 1]
       }
+    }
+    // clear data on index page
+    if (!this.$route.query.startAfter) {
+      this.products = []
+      this.lastPage = false
     }
     // generate products array
-    // this.products = productsSnapshot.docs.map((doc) => {
-    //   return { id: doc.id, ...doc.data() }
-    // })
-    // load more
-    if (this.$route.query.fullPath === '/p') {
-      // eslint-disable-next-line no-console
-      console.log('snap detect', typeof this.$route.fullPath)
-      this.products = []
-      this.snapshots = []
-    }
     for (const doc of productsSnapshot.docs) {
-      // if use back button check exist items
-      const found = this.products.some(el => el.id === doc.id)
-      if (!found) {
-        this.products.push({
-          id: doc.id,
-          ...doc.data()
-        })
-      }
+      this.products.push({
+        id: doc.id,
+        ...doc.data()
+      })
     }
   },
   watch: {
