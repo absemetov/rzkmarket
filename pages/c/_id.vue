eaad4f681226cc36a02b5049077bc8146a317429
Nadir <nadir@absemetov.org.ua>
2021-07-14 23:40:32 +0300

    home: filter old catalogs

--- pages/c/_id.vue
+++ pages/c/_id.vue
@@ -1,6 +1,6 @@
 <template>
   <v-alert>
-    <v-breadcrumbs :items="items" large />
+    <v-breadcrumbs :items="breadcrumbs" large />
     <ul>
       <li v-for="catalog of catalogs" :key="catalog.id">
         <h1>
@@ -17,19 +17,27 @@ export default {
   async asyncData ({ params, $fire }) {
     const catalogSnapshot = await $fire.firestore.collection('catalogs').doc(params.id).get()
     const catalog = { id: catalogSnapshot.id, ...catalogSnapshot.data() }
-    const items = []
+    const breadcrumbs = []
     if (catalog.parentId) {
-      items.push({ text: 'Back', to: { name: 'c-id', params: { id: catalog.parentId } } })
+      breadcrumbs.push({ text: 'Back', to: { name: 'c-id', params: { id: catalog.parentId } } })
     } else {
-      items.push({ text: 'Back', exact: true, to: { name: 'c' } })
+      breadcrumbs.push({ text: 'Back', exact: true, to: { name: 'c' } })
     }
-    items.push({ text: catalog.name, to: { name: 'c-id', params: { id: catalog.id } } })
+    breadcrumbs.push({ text: catalog.name, to: { name: 'c-id', params: { id: catalog.id } } })
     const catalogsSnapshot = await $fire.firestore.collection('catalogs').where('parentId', '==', catalog.id).orderBy('orderNumber').get()
     // generate catalogs array
-    const catalogs = catalogsSnapshot.docs.map((doc) => {
-      return { id: doc.id, ...doc.data() }
-    })
-    return { items, catalogs }
+    const catalogs = []
+    for (const catalog of catalogsSnapshot.docs) {
+      const catalogData = catalog.data()
+      // show current catalogs
+      if (catalogData.updatedAt === 1626290744) {
+        catalogs.push({ id: catalog.id, ...catalog.data() })
+      } else {
+        // Delete old catalogs (security problem????)
+        catalog.ref.delete()
+      }
+    }
+    return { breadcrumbs, catalogs }
   }
 }
 </script>
