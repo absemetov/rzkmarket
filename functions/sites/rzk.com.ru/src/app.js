6e284afd3ad6038c8d6fe542d4c8a7729b117ea4
Nadir <nadir@absemetov.org.ua>
2022-04-26 00:38:58 +0300

    test parcel

--- functions/sites/rzk.com.ru/src/app.js
+++ functions/sites/rzk.com.ru/src/app.js
@@ -1,1 +1,22 @@
-console.log("Hello world!");
+import algoliasearch from "algoliasearch/lite";
+import instantsearch from "instantsearch.js";
+import {searchBox, hits} from "instantsearch.js/es/widgets";
+
+const searchClient = algoliasearch("YZIFWJVE7R", "a56bd432142fc9813846fa737167eeef");
+
+const search = instantsearch({
+  indexName: "products",
+  searchClient,
+});
+
+search.addWidgets([
+  searchBox({
+    container: "#searchbox",
+  }),
+
+  hits({
+    container: "#hits",
+  }),
+]);
+
+search.start();
