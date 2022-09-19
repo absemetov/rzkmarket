1ae0b92e098c117b56c588e90a5ff523c9ce84a9
Nadir <nadir@absemetov.org.ua>
2022-09-19 23:35:58 +0300

    clear code

--- functions/index.js
+++ functions/index.js
@@ -3,18 +3,18 @@ const {initializeApp} = require("firebase-admin/app");
 // const functions = require("firebase-functions");
 // init firebase service
 initializeApp();
-const {botFunction} = require("./bot");
 const {siteFunction} = require("./sites/rzk.com.ru");
+const {botFunction} = require("./bot");
 const {productsUploadFunction} = require("./bot/bot_upload_scene");
 
-exports.rzkComUa = siteFunction;
-exports.rzkComRu = siteFunction;
-exports.botUa = botFunction;
-exports.botRu = botFunction;
-exports.productsUploadUa = productsUploadFunction;
-exports.productsUploadRu = productsUploadFunction;
-exports.triggersUa = require("./triggers");
-exports.triggersRu = require("./triggers");
+exports.site = siteFunction;
+// exports.rzkComRu = siteFunction;
+exports.bot = botFunction;
+// exports.botRu = botFunction;
+exports.productsUpload = productsUploadFunction;
+// exports.productsUploadRu = productsUploadFunction;
+exports.triggers = require("./triggers");
+// exports.triggersRu = require("./triggers");
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 // exports.helloWorld = functions.https.onRequest((request, response) => {
